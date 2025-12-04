898713999c66a064438f7814d3bde7ac00a70cc5
Alex Banul <albanul92@gmail.com>
2025-12-04 22:08:58 +0200

    feat: add day 4

---
+++ day4/task2/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, _ := os.Open("day4/task1/input.txt")
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	m := make([][]bool, 0)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		w := len(text)
+		line := make([]bool, w)
+
+		for j := 0; j < w; j++ {
+			line[j] = text[j:j+1] == "@"
+		}
+
+		m = append(m, line)
+	}
+
+	count := 0
+
+	for {
+		removed, nextRound := doRemoval(m)
+
+		if removed == 0 {
+			break
+		}
+
+		count += removed
+		m = nextRound
+	}
+
+	fmt.Println(count)
+}
+
+func doRemoval(m [][]bool) (count int, nextRound [][]bool) {
+	count = 0
+	nextRound = make([][]bool, len(m))
+
+	for i := 0; i < len(m); i++ {
+		nextRound[i] = make([]bool, len(m[i]))
+		for j := 0; j < len(m[i]); j++ {
+			if !m[i][j] {
+				//fmt.Print(".")
+				continue
+			}
+
+			nc := 0
+
+			// left
+			if j > 0 && m[i][j-1] {
+				nc++
+			}
+
+			// top-left
+			if j > 0 && i > 0 && m[i-1][j-1] {
+				nc++
+			}
+
+			// top
+			if i > 0 && m[i-1][j] {
+				nc++
+			}
+
+			// top-right
+			if i > 0 && j < len(m[i])-1 && m[i-1][j+1] {
+				nc++
+			}
+
+			// right
+			if j < len(m[i])-1 && m[i][j+1] {
+				nc++
+			}
+
+			// right-bottom
+			if i < len(m)-1 && j < len(m[i])-1 && m[i+1][j+1] {
+				nc++
+			}
+
+			// down
+			if i < len(m)-1 && m[i+1][j] {
+				nc++
+			}
+
+			// left-bottom
+			if i < len(m)-1 && j > 0 && m[i+1][j-1] {
+				nc++
+			}
+
+			if nc < 4 {
+				count++
+				//fmt.Print("x")
+			} else {
+				nextRound[i][j] = true
+				//fmt.Print("@")
+			}
+		}
+		//fmt.Println()
+	}
+	return count, nextRound
+}
